c4c712607df6e6f1e4fdb01feabd2708866bb7d6
MrChoppy <josephelhayek@hotmail.fr>
2024-03-23 12:44:37 -0400

    Now using DB + streak msgs

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ riot_api_key = "RGAPI-fee0395d-0f36-4539-b4ff-42058c196fc5"
 region = "na1"
 region_wide = "americas"
 #remove /                                      v                                v
-discord_bot_token = 'MTIxOTQyNzc4OTM1NTIyMTA0Mw.GFLqtG.RtTMHaH2siFEJQn11E1Gj93D-Bxe3iLtzwE6sg'
+discord_bot_token = 'MTIxOTQyNzc4OTM1NTIyMTA0Mw/.GFLqtG.RtTMHaH2siFEJQn11E1Gj93D/-Bxe3iLtzwE6sg'
