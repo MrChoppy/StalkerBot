e2aec366bf6e95de5eac812e2b670f944c619b95
Joseph El Hayek <josephelhayek@hotmail.fr>
2024-04-14 19:22:43 -0400

    Addes more commands and info about a match

--- discord_bot.py
+++ discord_bot.py
@@ -16,9 +16,8 @@ channel = None
 @client.event
 async def on_ready():
     DB.initialize_database()
-    check_game_status.start()
 
-    
+
 # Commands
 @client.command()
 async def stalk(ctx, *args):
@@ -58,7 +57,6 @@ async def unstalk(ctx, *args):
             return
         game_name, tag_line = arg.split("#")
 
-    
         db_summoner_data = DB.get_summoner(game_name, tag_line)
         if db_summoner_data:
             db_game_name, db_tag_line, db_summoner_puuid = db_summoner_data
@@ -72,6 +70,7 @@ async def unstalk(ctx, *args):
                 title="Who?",
                 description=f"You're not currently stalking {game_name}#{tag_line}",
                 color=discord.Color.dark_embed())
+
 
 @client.command()
 async def stalklist(ctx):
@@ -96,7 +95,9 @@ async def start(ctx, *args):
     global channel
     channel = ctx.channel
     await send_message("Bot started. Channel set here.")
- 
+    check_game_status.start()
+
+
 @tasks.loop(minutes=3)
 async def check_game_status():
     all_tracked_summoners = DB.get_all_summoners()
@@ -106,39 +107,43 @@ async def check_game_status():
             if game['in_game']:
                 if not was_in_game:
                     DB.set_was_in_game(summoner_puuid, True)
-                if gameId == None:
+                if gameId is None:
                     DB.set_game_id(summoner_puuid, game['gameId'])
             elif was_in_game:
-                if gameId != None:
+                if gameId is not None:
                     DB.set_was_in_game(summoner_puuid, False)
-                    await handle_game_result(summoner_puuid, game_name, gameId, consecutive_losses, consecutive_wins, time_wasted)
+                    await handle_game_result(summoner_puuid, game_name, gameId, consecutive_losses, consecutive_wins,
+                                             time_wasted)
 
 
 async def handle_game_result(summoner_puuid, game_name, gameId, consecutive_losses, consecutive_wins, time_wasted):
     game_result = Riot.check_last_game(gameId, summoner_puuid)
     if game_result is not None:
-        game_won, game_duration = game_result
-        game_duration_minutes = game_duration // 60
+        game_won, summoner_data = game_result
+        game_duration_minutes = summoner_data['duration'] // 60
 
         if game_won:
-           await handle_win(summoner_puuid, game_name, game_duration_minutes,consecutive_wins, time_wasted)
+            await handle_win(summoner_puuid, game_name, game_duration_minutes, consecutive_wins, time_wasted,
+                             summoner_data)
         else:
-           await handle_loss(summoner_puuid, game_name, game_duration_minutes,consecutive_losses, time_wasted)
+            await handle_loss(summoner_puuid, game_name, game_duration_minutes, consecutive_losses, time_wasted,
+                              summoner_data)
         DB.set_game_id(summoner_puuid, None)
 
 
-async def handle_win(summoner_puuid, game_name, game_duration_minutes,consecutive_wins, time_wasted):
+async def handle_win(summoner_puuid, game_name, game_duration_minutes, consecutive_wins, time_wasted, summoner_data):
     await send_embed(
         title="VICTORY",
         description=f"{game_name} won a game in {game_duration_minutes} minutes!!",
-        color=discord.Color.green()
+        color=discord.Color.green(),
+        summoner_data=summoner_data
     )
 
     if DB.get_consecutive_losses(summoner_puuid) > 0:
-            DB.set_consecutive_losses(summoner_puuid, 0)
-            DB.set_time_wasted(summoner_puuid, 0)
-            time_wasted = 0
-            
+        DB.set_consecutive_losses(summoner_puuid, 0)
+        DB.set_time_wasted(summoner_puuid, 0)
+        time_wasted = 0
+    DB.set_consecutive_losses(summoner_puuid, 0)
     consecutive_wins += 1
     time_wasted += game_duration_minutes
     DB.set_time_wasted(summoner_puuid, time_wasted)
@@ -148,23 +153,26 @@ async def handle_win(summoner_puuid, game_name, game_duration_minutes,consecutiv
         await send_embed(
             title="Win streak!",
             description=f"{game_name} has won {consecutive_wins} games in a row!",
-            color=discord.Color.blue()
+            color=discord.Color.blue(),
         )
-    
-        
 
-async def handle_loss(summoner_puuid, game_name, game_duration_minutes, consecutive_losses, time_wasted):
+
+async def handle_loss(summoner_puuid, game_name, game_duration_minutes, consecutive_losses, time_wasted, summoner_data):
     await send_embed(
         title="DEFEAT",
         description=f"{game_name} wasted {game_duration_minutes} minutes on a game just to lose",
-        color=discord.Color.red()
-        )
-        
+        color=discord.Color.red(),
+        summoner_data=summoner_data
+    )
+    print('loss')
     if DB.get_consecutive_wins(summoner_puuid) > 0:
+        print('loss consecutive wins if')
+
         DB.set_consecutive_wins(summoner_puuid, 0)
         DB.set_time_wasted(summoner_puuid, 0)
         time_wasted = 0
-        
+    DB.set_consecutive_wins(summoner_puuid, 0)
+
     time_wasted += game_duration_minutes
     consecutive_losses += 1
     DB.set_consecutive_losses(summoner_puuid, consecutive_losses)
@@ -174,8 +182,9 @@ async def handle_loss(summoner_puuid, game_name, game_duration_minutes, consecut
         await send_embed(
             title="Lose streak!",
             description=f"{game_name} has lost {consecutive_losses} games in a row for a grand total of {time_wasted} minutes!",
-            color=discord.Color.brand_red()
+            color=discord.Color.brand_red(),
         )
+
 
 # Message sending
 async def send_message(message):
@@ -183,11 +192,26 @@ async def send_message(message):
     await channel.send(message)
 
 
-async def send_embed(title, description, color):
+async def send_embed(title, description, color, summoner_data=None):
     global channel
     embed = discord.Embed(
         title=title,
         description=description,
         color=color
     )
+    if summoner_data:
+        embed.add_field(name="Role", value=summoner_data['teamPosition'], inline=True)
+        embed.add_field(name="Champion", value=summoner_data['championName'], inline=True)
+        embed.add_field(name="Kills", value=str(summoner_data['kills']), inline=True)
+        embed.add_field(name="Deaths", value=str(summoner_data['deaths']), inline=True)
+        embed.add_field(name="Assists", value=str(summoner_data['assists']), inline=True)
+        embed.add_field(name="Gold Earned", value=str(summoner_data['goldEarned']), inline=True)
+        embed.add_field(name="Minions Killed", value=str(summoner_data['minionKilled']), inline=True)
+        embed.add_field(name="Wards Placed", value=str(summoner_data['wardsPlaced']), inline=True)
+        embed.add_field(name="Damage Dealt", value=str(summoner_data['damageDealt']), inline=True)
+        embed.add_field(name="Damage Taken", value=str(summoner_data['damageTaken']), inline=True)
+        embed.add_field(name="Damage Struct.", value=str(summoner_data['damageBuildings']), inline=True)
+        embed.add_field(name="Side", value=str(summoner_data['side']), inline=True)
+        embed.add_field(name="GD@15", value=str(summoner_data['goldDifference']), inline=True)
+
     await channel.send(embed=embed)
--- riot_api.py
+++ riot_api.py
@@ -13,7 +13,6 @@ def get_summoner_puuid(game_name, tag_line):
         data = response.json()
         return data
     except requests.exceptions.RequestException as e:
-        print(f"Error in get_summoner_puuid from Riot API: {e}")
         return None
     except json.decoder.JSONDecodeError as e:
         print(f"Error decoding JSON response: {e}")
@@ -29,9 +28,10 @@ def get_current_game(puuid):
         if data["gameType"] == "MATCHED" and data["gameMode"] == "CLASSIC":
             return {"in_game": True, "gameId": data["gameId"]}
     except requests.exceptions.RequestException as e:
+        print(f"Game could not be found or api failed")
         return {"in_game": False}
     except json.decoder.JSONDecodeError as e:
-        print(f"Error decoding JSON response: {e}")
+        print(f"Json decode error")
         return None
 
 
@@ -42,18 +42,45 @@ def currect_game(game, puuid):
         print(game['gameLength'])
 
 
-def check_last_game(gameId, puuid):
-    url = f"https://{region_wide}.api.riotgames.com/lol/match/v5/matches/NA1_{gameId}?api_key={riot_api_key}"
+def check_last_game(game_id, puuid):
+    url = f"https://{region_wide}.api.riotgames.com/lol/match/v5/matches/NA1_{game_id}?api_key={riot_api_key}"
     try:
         response = requests.get(url)
         response.raise_for_status()
         game = response.json()
 
         summoner = find_participant_by_puuid(game, puuid)
-        if summoner['win'] == True:
-            return True, game['info']['gameDuration']
-        else:
-            return False, game['info']['gameDuration']
+        team_position = summoner['teamPosition']
+        side = 'idk'
+        gold_difference = get_gold_difference(game_id, puuid)
+        if summoner['role'] == 'SUPPORT' or summoner['role'] == 'CARRY':
+            team_position = summoner['role']
+        if summoner['teamId'] == 100:
+            side = 'Blue'
+        elif summoner['teamId'] == 200:
+            side = 'Red'
+
+        if gold_difference > 0:
+            gold_difference = f'+{gold_difference}'
+
+        summoner_data = {
+            'teamPosition': team_position,
+            'championName': summoner['championName'],
+            'deaths': summoner['deaths'],
+            'kills': summoner['kills'],
+            'assists': summoner['assists'],
+            'damageDealt': summoner['totalDamageDealtToChampions'],
+            'damageTaken': summoner['totalDamageTaken'],
+            'wardsPlaced': summoner['wardsPlaced'],
+            'minionKilled': summoner['totalMinionsKilled'] + summoner['totalEnemyJungleMinionsKilled'] + summoner[
+                'totalAllyJungleMinionsKilled'],
+            'damageBuildings': summoner['damageDealtToBuildings'],
+            'goldEarned': summoner['goldEarned'],
+            'side': side,
+            'goldDifference': gold_difference,
+            'duration': game['info']['gameDuration'],
+        }
+        return summoner['win'], summoner_data
 
     except requests.exceptions.RequestException as e:
         print(f"Error in check_last_game from Riot API: {e}")
@@ -67,3 +94,35 @@ def find_participant_by_puuid(game, puuid):
         if participant['puuid'] == puuid:
             return participant
     return None
+
+
+# alternative name get_match_timeline
+def get_gold_difference(game_id, puuid):
+    url = f'https://{region_wide}.api.riotgames.com/lol/match/v5/matches/{game_id}/timeline?api_key={riot_api_key}'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        timeline_data = response.json()
+        timeline_data = timeline_data['info']
+
+        main_player = 0
+        opponent_player = 0
+        gold_difference = 0
+        for participant in timeline_data['participants']:
+            if participant['puuid'] == puuid:
+                main_player = participant['participantId']
+                if main_player > 5:
+                    opponent_player = main_player - 5
+                elif main_player <= 5:
+                    opponent_player = main_player + 5
+
+        for frame in timeline_data['frames']:
+            if frame['timestamp'] >= 900000:
+                gold_difference = frame['participantFrames'][str(main_player)]['totalGold'] - \
+                                  frame['participantFrames'][str(opponent_player)]['totalGold']
+                break
+        return gold_difference
+
+    else:
+        print("Error in gold difference")
+        return 0
