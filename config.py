50d35e4a1005a33d1b0c257c28c745fadedf0d1b
Joseph El Hayek <josephelhayek@hotmail.fr>
2024-04-27 04:58:39 -0400

    ????????????

--- config.py
+++
@@ -1,5 +0,0 @@
-riot_api_key = "RGAPI-fee0395d-0f36-4539-b4ff-42058c196fc5"
-region = "na1"
-region_wide = "americas"
-#remove /                                      v                                v
-discord_bot_token = 'MTIxOTQyNzc4OTM1NTIyMTA0Mw/.Gn4Zmh.Yw0EibP3KbOAGoZmsqNxHoah/-L_Wj1KxPx_ZDI'
